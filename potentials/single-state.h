57452ae44cd76fc157de4bf0ceb6cd2f3779df66
Greg Medders <grmedders@gmail.com>
2017-11-06 10:25:37 -0500

    Initial work on single_state class. Mirrors surface hopping, without the SH functionality

---
+++ potentials/single-state.h
@@ -0,0 +1,36 @@
+#ifndef SINGLE_STATE_H
+#define SINGLE_STATE_H
+
+#include <cstdlib>
+
+#include <armadillo>
+
+#include "explicit-bath.h"
+
+namespace pot {
+
+struct single_state : public explicit_bath {
+
+    arma::ivec state_id;
+
+    double force(size_t, size_t, size_t, const double*, double*);
+    virtual double VAA(const double* x, double* f) = 0;
+    virtual void set_params(double*) = 0;
+
+    void set_all_bead_states(const int, int);
+    void set_individual_bead_states(std::vector<int>&);
+
+    double avg_active_state(){ return 0; };
+    double sum_active_state(){ return 0; };
+
+private:
+    bool init_pot = false;
+    int m_nbead;
+
+    void check_allocation(size_t, arma::ivec&);
+
+};
+
+} // namespace pot
+
+#endif // SINGLE_STATE_H
