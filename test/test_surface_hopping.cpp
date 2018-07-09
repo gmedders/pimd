e7e64380e1392a2728e480d5a00f9e76de6bfdf5
Greg Medders <grmedders@gmail.com>
2018-07-09 00:35:32 -0400

    Finished unit test coverage of surface_hopping

--- test/test_surface_hopping.cpp
+++ test/test_surface_hopping.cpp
@@ -10,52 +10,14 @@
 // in it. We'll create instances of the "double_well" class, which is the
 // simplest child of surface_hopping, to test.
 
-// bool init_pot = false;
-// bool init_hop = false;
-// double Gamma;
-// double dt;
-// double beta;
-// double voltage;
-//
-// arma::ivec state_id;
-//
-// int nhops;
-//
-// double force(size_t, size_t, size_t, const double *, double *);
-// virtual double VAA(const double *x, double *f) = 0;
-// virtual double VBB(const double *x, double *f) = 0;
-// virtual void set_params(double *) = 0;
-//
-// void check_allocation(size_t, arma::ivec &);
-//
-// double fermi_function(const double, const double);
-// double hop_probability(const double, int);
-// void hop();
-//
-// void set_all_bead_states(const int, int);
-// void set_individual_bead_states(std::vector<int> &);
-// void set_hopping_params(double *);
-//
-// double avg_active_state();
-// double sum_active_state();
-//
-// void print_state_params();
-
-double omega(0.0009765625); // omega
-double atm_mass(2000);      // au
-double param_g(3.9);        // barrier = 2w
-double dG(-0.003906252);
-double params[] = {omega, atm_mass, param_g, dG};
+double GammaEl(1.0);
+double dt(2.0);
+double beta(1024.0);
+double voltage(0.0001);
+double hop_params[] = {GammaEl, dt, beta, voltage};
 
 TEST(surface_hopping, hopping_params) {
   pot::double_well my_pot;
-
-  double GammaEl(1.0);
-  double dt(2.0);
-  double beta(1024.0);
-  double voltage(3.0);
-  double hop_params[] = {GammaEl, dt, beta, voltage};
-
   my_pot.set_hopping_params(hop_params);
 
   EXPECT_EQ(my_pot.Gamma, GammaEl);
@@ -88,4 +50,48 @@ TEST(surface_hopping, set_individual_bead_states) {
   for (int i = 0; i < nbead; ++i) {
     EXPECT_EQ(my_pot.state_id[i], state_ids[i]);
   }
+}
+
+TEST(surface_hopping, fermi_function) {
+  double mu(-0.02);
+  double dE(0.001);
+
+  pot::double_well my_pot;
+  my_pot.set_hopping_params(hop_params);
+
+  double f_mu0 = my_pot.fermi_function(dE, 0.0);
+  EXPECT_DOUBLE_EQ(f_mu0, 0.26424898168976962);
+  double f_mu = my_pot.fermi_function(dE, mu);
+  EXPECT_DOUBLE_EQ(f_mu, 4.5806958984748794e-10);
+  double f_huge_dE = my_pot.fermi_function(1000, mu);
+  EXPECT_DOUBLE_EQ(f_huge_dE, 0.0);
+}
+
+TEST(surface_hopping, hop_probability) {
+  double dE(0.001);
+  int active_state(0);
+
+  pot::double_well my_pot;
+  my_pot.set_hopping_params(hop_params);
+
+  double p_hop_0 = my_pot.hop_probability(dE, 0);
+  EXPECT_DOUBLE_EQ(p_hop_0, 0.52873820111413861);
+  double p_hop_1 = my_pot.hop_probability(dE, 1);
+  EXPECT_DOUBLE_EQ(p_hop_1, 1.4712617988858614);
+}
+
+TEST(surface_hopping, sum_active_state) {
+  std::vector<int> state_ids = {0, 1, 0, 0, 0, 1, 0, 0};
+  const int nbead = state_ids.size();
+
+  pot::double_well my_pot;
+  my_pot.set_individual_bead_states(state_ids);
+
+  EXPECT_EQ(my_pot.state_id.size(), nbead);
+  int sum(0);
+  for (int i = 0; i < nbead; ++i) {
+    sum += my_pot.state_id[i];
+  }
+  EXPECT_EQ(my_pot.sum_active_state(), sum);
+  EXPECT_EQ(my_pot.avg_active_state(), double(sum) / double(nbead));
 }
