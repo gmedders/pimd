8a02febe9231890f12282462822eccd8cf770af1
Greg Medders <grmedders@gmail.com>
2018-07-14 18:26:55 -0400

    Moved finite differences function into a header file

--- test/test_sho.cpp
+++ test/test_sho.cpp
@@ -3,32 +3,13 @@
 #include <stdexcept>
 #include <string>
 
+#include "finite-difference.h"
 #include "sho.h"
 #include "gtest/gtest.h"
 
 // single_state is an abstract class, so there are only a few things to test
 // in it. We'll create an instance of the "sho" class, which is the simplest
 // child of single_state, to test.
-
-template <class potential>
-void run_finite_differences(potential my_pot, std::vector<double> &crd,
-                            std::vector<double> &f) {
-  double dx = 5.0E-7;
-  std::vector<double> weight = {1.0 / 12.0, -2.0 / 3.0, 2.0 / 3.0, -1.0 / 12.0};
-  std::vector<double> displacement = {-2.0 * dx, -1.0 * dx, 1.0 * dx, 2.0 * dx};
-  std::vector<double> dummy_grad = f;
-
-  for (std::vector<double>::size_type i(0); i < crd.size(); ++i) {
-    double grad(0.0);
-    double orig(crd[i]);
-    for (std::vector<double>::size_type j(0); j < displacement.size(); ++j) {
-      crd[i] = orig + displacement[j];
-      grad += weight[j] * my_pot.VAA(&crd[0], &dummy_grad[0]);
-    }
-    crd[i] = orig;
-    f[i] = -grad / dx;
-  }
-}
 
 double omega(0.001);   // omega
 double atm_mass(2000); // au
