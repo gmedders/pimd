760d7e61da3e139bf55b52151ad635abc8cf0d65
Greg Medders <grmedders@gmail.com>
2018-07-15 01:27:03 -0400

    Finished velocity-verlet test

--- test/test_vv.cpp
+++ test/test_vv.cpp
@@ -6,9 +6,11 @@
 #include "sim_classes.h"
 #include "gtest/gtest.h"
 
-// surface_hopping is an abstract class, so there are only a few things to test
-// in it. We'll create instances of the "double_well" class, which is the
-// simplest child of surface_hopping, to test.
+// This velocity_verlet test assumes the following is set up as the potential:
+// typedef pot::sho potential_type;
+// static double omega(0.001);   // omega
+// static double atm_mass(2000); // au
+// static double params[] = {omega, atm_mass};
 
 TEST(velocity_verlet, make_one_step) {
   double pos[] = {0.1};
@@ -24,77 +26,28 @@ TEST(velocity_verlet, make_one_step) {
   parts::vv sim;
   sim.set_up(nbead, ndim, natom, beta, dt, pos, vel);
 
+  EXPECT_EQ(sim.ndofs(), 1);
+
   EXPECT_DOUBLE_EQ(sim.invariant(), 0.10001);
   EXPECT_DOUBLE_EQ(sim.Ep(), 1e-05);
+  EXPECT_DOUBLE_EQ(sim.Ek(), 0.1000000000000000);
+  EXPECT_DOUBLE_EQ(sim.Espring(), 0.0);
   EXPECT_DOUBLE_EQ(sim.temp_kT(), 0.0);
   EXPECT_DOUBLE_EQ(sim.avg_cart_pos(), 0.10000000000000001);
+  EXPECT_DOUBLE_EQ(sim.L1_cart_pos(), 0.0);
+  EXPECT_DOUBLE_EQ(sim.L2_cart_pos(), 0.0);
+  EXPECT_DOUBLE_EQ(sim.Linf_cart_pos(), 0.0);
 
   // Make one step
   sim.step(dt);
 
   EXPECT_DOUBLE_EQ(sim.invariant(), 0.10000999999952499);
   EXPECT_DOUBLE_EQ(sim.Ep(), 8.0999910000025029e-06);
+  EXPECT_DOUBLE_EQ(sim.Ek(), 0.10000190000852499);
+  EXPECT_DOUBLE_EQ(sim.Espring(), 0.0);
   EXPECT_DOUBLE_EQ(sim.temp_kT(), 0.20000380001704998);
   EXPECT_DOUBLE_EQ(sim.avg_cart_pos(), 0.089999950000000009);
+  EXPECT_DOUBLE_EQ(sim.L1_cart_pos(), 0.0);
+  EXPECT_DOUBLE_EQ(sim.L2_cart_pos(), 0.0);
+  EXPECT_DOUBLE_EQ(sim.Linf_cart_pos(), 0.0);
 }
-
-// inline size_t ndofs() const;
-//
-// inline double *cart_ptr() { return m_pos.memptr(); };
-//
-// virtual double force(const double *, double *) = 0;
-//
-// void init(size_t ndof, const double &dt, const double *mass,
-//           const double *cartpos, const double *cartvel);
-//
-// void step(const double &);
-// double invariant() const;
-//
-// protected:
-// double m_dt;
-// size_t m_ndofs;
-//
-// arma::vec m_pos;
-// arma::vec m_mom;
-// arma::vec m_frc;
-// arma::vec m_mass;
-//
-// double m_Epot;
-// double m_Ekin;
-// double m_temp_kT;
-
-// void set_up_new_init_cond(const size_t, const size_t, const size_t,
-//                           const double, const double);
-// void set_up_new_init_cond(const size_t, const size_t, const size_t,
-//                           const double, const double, double *);
-// void set_up(const size_t, const size_t, const size_t, const double,
-//             const double, double *, double *);
-// double force(const double *, double *);
-//
-// inline double Espring() const { return 0.0; }
-// inline double Ep() const { return m_Epot; }
-// inline double Ek() const { return m_Ekin; }
-// inline double temp_kT() const { return m_temp_kT; }
-// double avg_cart_pos() {
-//   calc_pos_stats();
-//   return m_avg_cart_pos;
-// };
-// // double avg_cart_pos() const { return m_avg_cart_pos; };
-// double L1_cart_pos() const { return m_L1_cart_pos; };
-// double L2_cart_pos() const { return m_L2_cart_pos; };
-// double Linf_cart_pos() const { return m_Linf_cart_pos; };
-// void calc_pos_stats(void);
-//
-// void print_params();
-//
-// potential_type m_potential;
-//
-// private:
-// double m_avg_cart_pos;
-// double m_L1_cart_pos;
-// double m_L2_cart_pos;
-// double m_Linf_cart_pos;
-//
-// size_t m_natom;
-// size_t m_ndim;
-// size_t m_ndofs;
